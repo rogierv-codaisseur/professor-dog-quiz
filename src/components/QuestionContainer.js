d7f114ded40e32aab65200e1d798ba978c83dea8
Rogier Verkaik <rogierv@gmail.com>
2019-01-28 16:59:16 +0100

    Initial setup

---
+++ src/components/QuestionContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Question from './Question';
+
+export default class QuestionContainer extends Component {
+  render() {
+    return <Question />;
+  }
+}
---
+++ src/components/StatsStreakHighest.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class StatsStreakHighest extends Component {
+  render() {
+    return <div>Stats</div>;
+  }
+}
