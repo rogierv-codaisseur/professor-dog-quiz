10ca7eafdc9eb206631f40bdc0d13be123df1471
EmineBC <eminebahce@gmail.com>
2019-01-28 18:05:43 +0100

    fetch data

--- src/components/DogQuiz.js
+++ src/components/DogQuiz.js
@@ -3,14 +3,12 @@ import QuestionContainer from './QuestionContainer';
 import AnswerContainer from './AnswersContainer';
 import StatsContainer from './StatsContainer';
 
-export default class DogQuiz extends Component {
-	render() {
-		return (
-			<div>
-				<QuestionContainer />
-				<AnswerContainer />
-				<StatsContainer />
-			</div>
-		);
-	}
+export default function(props) {
+	const dogs = props.dogs;
+	return (
+		<div>
+			<h1>Dogs list</h1>
+			<p>{props.dogs}</p>
+		</div>
+	);
 }
