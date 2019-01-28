6032f6c2b58b819dd88c70358e95b719790204e0
Rogier Verkaik <rogierv@gmail.com>
2019-01-28 18:00:06 +0100

    Add logo

--- src/components/DogQuiz.js
+++ src/components/DogQuiz.js
@@ -3,11 +3,13 @@ import QuestionContainer from './QuestionContainer';
 import AnswerContainer from './AnswersContainer';
 import StatsContainer from './StatsContainer';
 import './DogQuiz.css';
+import logo from './../images/dog-api-logo.svg';
 
 export default class DogQuiz extends Component {
   render() {
     return (
       <div className='dog-quiz'>
+        <img src={logo} alt='logo' />
         <h1>Professor Dog Quiz</h1>
         <QuestionContainer />
         <AnswerContainer />
