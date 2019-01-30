6ad4eae84427132c25c099b7538b76ed35f84ab8
MannRobertJ <mann.robertj@gmail.com>
2019-01-30 10:51:34 +0100

    fixed actions

--- src/actions/dogs.js
+++ src/actions/dogs.js
@@ -1,58 +1,60 @@
-import * as request from 'superagent';
+import * as request from "superagent";
+
 
 function randomDogs(dogs) {
-	const randomDog = dogs[Math.floor(Math.random() * dogs.length)];
-	const newDogs = dogs.filter(dog => dog !== randomDog);
-	const randomDogName = newDogs[Math.floor(Math.random() * newDogs.length)];
-	const newNewDogs = newDogs.filter(dog => dog !== randomDogName);
-	const otherRandomDogName = newNewDogs[Math.floor(Math.random() * newNewDogs.length)];
-	return [randomDog, randomDogName, otherRandomDogName];
+  const randomDog = dogs[Math.floor(Math.random() * dogs.length)];
+  const newDogs = dogs.filter(dog => dog !== randomDog);
+  const randomDogName = newDogs[Math.floor(Math.random() * newDogs.length)];
+  const newNewDogs = newDogs.filter(dog => dog !== randomDogName);
+  const otherRandomDogName =
+    newNewDogs[Math.floor(Math.random() * newNewDogs.length)];
+  return [randomDog, randomDogName, otherRandomDogName];
 }
 
 export function getDogs() {
-	return function(dispatch) {
-		request
-			.get('https://dog.ceo/api/breeds/list/all')
-			.then(response => response.body.message)
-			.then(dogs => {
-				dispatch(showDogsList(dogs));
-				return dogs;
-			})
-			.then(randomNewDog => randomDogs(Object.keys(randomNewDog)))
-			.then(breedName => dispatch(getBreedPhoto(breedName)));
-	};
+  return function(dispatch) {
+    request
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then(response => response.body.message)
+      .then(dogs => {
+        dispatch(showDogsList(dogs));
+      });
+  };
 }
 
-const SHOW_DOGS = 'SHOW_DOGS';
+
+const SHOW_DOGS = "SHOW_DOGS";
 
 export function showDogsList(dogs) {
-	return {
-		type: SHOW_DOGS,
-		payload: {
-			dogs: dogs,
-		},
-	};
+  return {
+    type: SHOW_DOGS,
+    payload: {
+      dogs: dogs
+    }
+  };
 }
 
-const SHOW_PHOTO = 'SHOW_PHOTO';
-
-export function getBreedPhoto(breedName) {
-	return function(dispatch) {
-		request
-			.get(`https://dog.ceo/api/breed/${breedName[0]}/images/random`)
-			.then(response => dispatch(showBreedPhoto(breedName, response.body.message)));
-	};
+export function getRandomDogsAndPhoto(allDogs) {
+	const threeRandomDogs = randomDogs(Object.keys(allDogs))
+  return function(dispatch) {
+    request
+      .get(`https://dog.ceo/api/breed/${threeRandomDogs[0]}/images/random`)
+      .then(response =>
+        dispatch(sendRandomDogsWithPhoto(threeRandomDogs, response.body.message))
+      );
+  };
 }
 
-export function showBreedPhoto(breedName, url) {
+
+export function sendRandomDogsWithPhoto(randomDogs, url) {
 	//console.log(breedName);
 	return {
-		type: SHOW_PHOTO,
-		payload: {
-			goodDog: breedName[0],
-			badDog1: breedName[1],
-			badDog2: breedName[2],
-			imageUrl: url,
-		},
+	  type: "SHOW_PHOTO",
+	  payload: {
+		goodDog: randomDogs[0],
+		badDog1: randomDogs[1],
+		badDog2: randomDogs[2],
+		imageUrl: url
+	  }
 	};
-}
+  }
--- src/components/DogQuizContainer.js
+++ src/components/DogQuizContainer.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react';
-import DogQuiz from './DogQuiz.js';
-import { getDogs, getBreedPhoto } from '../actions/dogs';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import DogQuiz from "./DogQuiz.js";
+import {
+  getDogs,
+  getRandomDogsAndPhoto,
+} from "../actions/dogs";
+import { connect } from "react-redux";
 
 class DogQuizContainer extends Component {
-	state = {};
+  state = { loading: true };
 
-	componentDidMount = () => {
-		this.props.getDogs();
-	};
+  componentDidMount = () => {
+    this.props.getDogs();
+  };
 
-	render() {
-		if (!this.props.image) return 'Loading photos...';
-		return <DogQuiz image={this.props.image} dogs={this.props.dogs} />;
-	}
+  
+  componentDidUpdate = (prevProps) => {
+	if (this.props.image === prevProps.image) {
+		this.props.getRandomDogsAndPhoto(this.props.dogs[0].dogs);
+	}    
+  }; 
+
+  render() {
+    if (!this.props.image) return "Loading photos...";
+    return <DogQuiz image={this.props.image} dogs={this.props.dogs} />;
+  }
 }
 
 const mapStateToProps = state => ({
-	dogs: state.dogs,
-	image: state.images,
+  dogs: state.dogs,
+  image: state.images,
+  turn: state.successRate
 });
 
 export default connect(
-	mapStateToProps,
-	{ getDogs, getBreedPhoto }
+  mapStateToProps,
+  { getDogs, getRandomDogsAndPhoto }
 )(DogQuizContainer);
-
-/**
- * 
-function getRandomName(dogs) {
-	const dog = dogs[Math.floor(Math.random() * dogs.length)];
-	const newDogs = dogs.filter(dog => dog !== dog);
-	const randomDogName = newDogs[Math.floor(Math.random() * newDogs.length)];
-	const newNewDogs = newDogs.filter(dog => dog !== randomDogName);
-	const otherRandomDogName = newNewDogs[Math.floor(Math.random() * newNewDogs.length)];
-  return [dog, randomDogName, otherRandomDogName];
-}
-	componentWillReceiveProps = () => {
-		this.getRandomName(Object.keys(this.props.dogs.dogs), this.props.image.name);
-  };
-  
-  //this.getRandomName(Object.keys(this.props.dogs.dogs), this.props.image.name);
- */
