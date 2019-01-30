e7fd6df9134e67f1f80d275101f910102022de52
MannRobertJ <mann.robertj@gmail.com>
2019-01-30 16:57:39 +0100

    DONE, maybe?

--- src/components/DogQuizContainer.js
+++ src/components/DogQuizContainer.js
@@ -43,6 +43,7 @@ class DogQuizContainer extends Component {
         this.props.currentStreak === prevProps.currentStreak &&
         (this.props.turn === 0 || this.props.turn !== prevProps.turn))
     ) {
+      console.log("git being annoying again");
       this.props.getRandomDogsAndPhoto({
         ...this.props.availableDogs.available
       });
